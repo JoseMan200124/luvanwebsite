53cf354dcfa648f8272713da392c64b3bcb0a452
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2025-01-02 03:02:52 -0600

    Configuracion
    Fix, se corrige el entorno de axios

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,11 +1,18 @@
 // axiosConfig.js
 import axios from 'axios';
+import https from 'https'; // Importar el módulo HTTPS
 
 const API_URL = 'https://34.56.161.166/api';
+
+// Crear un agente HTTPS que deshabilite la validación del certificado
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false, // Desactiva la validación del certificado
+});
 
 // Crear una instancia de Axios con la configuración base
 const api = axios.create({
     baseURL: API_URL,
+    httpsAgent, // Asignar el agente HTTPS personalizado
 });
 
 // Opcional: Configurar interceptores para manejar tokens u otras configuraciones globales
