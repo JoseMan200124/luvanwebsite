42db68ef48c3e6ad69b5b3ed2f87ccaa34332799
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2025-02-08 22:04:46 -0600

    Ruta
    Add, se añade la ruta de producción

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,17 +1,15 @@
 // src/utils/axiosConfig.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://api.transportesluvan.com/api';
 
-// Crear una instancia de Axios con la configuración base
 const api = axios.create({
     baseURL: API_URL,
 });
 
-// Opcional: Configurar interceptores para manejar tokens u otras configuraciones globales
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token'); // O de donde obtengas el token
+        const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
